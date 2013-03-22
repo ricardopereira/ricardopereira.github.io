954379459fe82c59908c2e3379111d4304c87843
Ricardo Pereira <rikpereira@gmail.com>
2013-03-22 02:16:08 +0000

    Experiments

--- test/script.js
+++ test/script.js
@@ -2,7 +2,11 @@ var slider = Swipe(document.getElementById('slider'),
   {
 
   });
-  
-$(document).ready(function () {
-  $('#logo').delay(800).fadeIn(400);
+
+$(document).ready(function() { 
+$('#logo').delay(1000).fadeIn('slow');
+});
+
+$(window).load(function() {
+console.log("Window loaded");
 });
