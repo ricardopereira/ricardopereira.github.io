1044670d7e3f0f8096d95fb6f3c5ce8d9208d55e
Ricardo Pereira <rikpereira@gmail.com>
2013-03-22 23:38:12 +0000

    Experiments

--- test/js/script.js
+++ test/js/script.js
@@ -1,5 +1,6 @@
 var slider = Swipe(document.getElementById('slider'),
   {
+/*
     callback: function(pos)
     {
       if (pos == 1)
@@ -7,6 +8,7 @@ var slider = Swipe(document.getElementById('slider'),
       else
         $('.contacts').show();
     }
+*/
   });
 
 $(document).ready(function() { 
