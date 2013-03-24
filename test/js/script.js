e4d4924a5bd514ab345a352e0484fef7efe1fecc
Ricardo Pereira <rikpereira@gmail.com>
2013-03-24 23:43:33 +0000

    Experiments

--- test/js/script.js
+++ test/js/script.js
@@ -1,15 +1,16 @@
 var slider = Swipe(document.getElementById('slider'),
   {
-/*
-    callback: function(pos)
-    {
-      if (pos == 1)
-        $('.contacts').hide();
-      else
-        $('.contacts').show();
+    callback: function(pos) {
+      
+      var i = bullets.length;
+      while (i--) {
+        bullets[i].className = ' ';
+      }
+      bullets[pos].className = 'on';
     }
-*/
   });
+  
+var bullets = document.getElementById('position').getElementsByTagName('li');
   
 var mslider = Swipe(document.getElementById('slider-mobile'), { });
 
